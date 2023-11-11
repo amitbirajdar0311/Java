93dc8eb454cd760f70fa1c08741113eabe758687
amit1711040@gmail.com <amit1711040@gmail.com>
2023-11-11 23:55:33 +0530

    Life will change from here

---
+++ 5.Variable_And_DataTypes/BoilerPlate.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class BoilerPlate{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World");
+        sc.close();
+    }
+}

3b66402622788a6884323c807b4140af486b86bf
amit1711040@gmail.com <amit1711040@gmail.com>
2023-11-14 18:42:00 +0530

    Patterns completed successfully

--- 9.Pattern/HW/HollowRombosPatter.java
+++ 9.Pattern/HW/HollowRombosPatter.java
@@ -1,21 +1,21 @@
+package HW;
 import java.util.*;
-public class HollowPattern {
+public class HollowRombosPatter {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of rows");
         int n = sc.nextInt();
-        System.out.println("Enter number of columns");
-        int k = sc.nextInt();
-
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=k;j++){
-                if(i==1 || i==n || j==1 || j == k ){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
                     System.out.print("*");
                 }
                 else{
                     System.out.print(" ");
                 }
-               
             }
 
             System.out.println(" ");
---
+++ 9.Pattern/HW/SolidRombus.java
@@ -0,0 +1,19 @@
+package HW;
+import java.util.*;
+public class SolidRombus {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+    }
+}
